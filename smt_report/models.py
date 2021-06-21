95efc0c5a6c2aad27f821e2da626ee9b25f5ac76
jackyhsu <jacku_hsu@genorx.com>
2021-06-21 17:46:53 +0800

    add fields

--- smt_report/models.py
+++ smt_report/models.py
@@ -5,5 +5,7 @@ from extra_table.models import Stores
 class smt_report(models.Model):
     date = models.DateField(blank=True, null=True)
     sales = models.PositiveIntegerField(blank=True, null=True)
+    sales_budget = models.PositiveIntegerField(blank=True, null=True)
     margin = models.PositiveIntegerField(blank=True, null=True)
+    margin_budget = models.PositiveIntegerField(blank=True, null=True)
     stores = models.ForeignKey(Stores, on_delete=models.CASCADE)
