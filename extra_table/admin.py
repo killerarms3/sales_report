fcdc3eca33dec239233f3faaeb1376c7d8a12a82
dytk2134 <dytk2134@gmail.com>
2021-06-30 16:55:47 +0800

    add navbar and register extra_table models to the Django admin

--- extra_table/admin.py
+++ extra_table/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from extra_table.models import Stores, SKU, Store_house, CRM_tags
 # Register your models here.
+
+@admin.register(Stores)
+class StoresAdmin(admin.ModelAdmin):
+    list_display = (
+        'code',
+        'name',
+        'category',
+        'subtype'
+    )
+
+@admin.register(SKU)
+class SKUAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'cost',
+        'status'
+    )
+
+@admin.register(Store_house)
+class StoreHouseAdmin(admin.ModelAdmin):
+    list_display = (
+        'code',
+        'name',
+        'category',
+        'subtype'
+    )
+
+@admin.register(CRM_tags)
+class CRMTagAdmin(admin.ModelAdmin):
+    list_display = (
+        'tag',
+        'category',
+        'subtype'
+    )
